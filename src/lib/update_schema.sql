8383ff4c2e17142c0a4990badad1b62d6cbbc850
Fer-Bar <linofernando2703@gmail.com>
2026-01-23 02:13:01 -0400

    deleted .sql file

--- src/lib/update_schema.sql
+++
@@ -1,6 +0,0 @@
--- SQL Migration to add new fields to restrooms table
-ALTER TABLE restrooms 
-ADD COLUMN IF NOT EXISTS opening_time time,
-ADD COLUMN IF NOT EXISTS closing_time time,
-ADD COLUMN IF NOT EXISTS description text,
-ADD COLUMN IF NOT EXISTS photos text[] DEFAULT '{}';
